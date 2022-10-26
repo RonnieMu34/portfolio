37d59a97b7fcf7350b933b240ba351fe20d5d210
RonnieMu34 <ronnie34mu@gmail.com>
2022-10-26 11:05:13 -0400

    Create particle class with dimensions for particles

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let ParticlesArray = [];
+
+class Particle {
+    constructor() {
+        this.size = Math.random() * 5 + 3;
+        this.x = Math.random() * (canvas.width - 50) + (this.size + 10);
+        this.y = Math.random() * (canvas.height - 50) + (this.size + 10);
+        this.directionX = Math.random() * 5 - 2.5;
+        this.directionY = Math.random() * 5 - 2.5;
+    }
+}
